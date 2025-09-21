754565b7ae8e93e98dd619f78663df473be78ac2
Brinedew <brinedew@proton.me>
2025-09-21 18:04:11 +0700

    vault backup: 2025-09-21 18:04:11

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "brinedew.com",
+    baseUrl: "brinedew.bio",
     ignorePatterns: [
       // existing
       "private",
