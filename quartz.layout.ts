2170eb4428a85b63f7e47d61c7df90040513b56f
Brinedew <brinedew@proton.me>
2025-12-14 19:40:26 +0700

    feat: add Support me link to footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -10,6 +10,7 @@ export const sharedPageComponents: SharedLayout = {
     links: {
       // Use an absolute link so it doesn't change based on current page depth
       "About": "/About.html",
+      "Support me": "/Support-me.html",
     },
   }),
 }
